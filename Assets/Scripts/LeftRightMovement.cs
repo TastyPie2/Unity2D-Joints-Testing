422a2e5ea0bfb81510b7f4a329bf2e8023787cbd
lesbian-amanda-young <123447840+lesbian-amanda-young@users.noreply.github.com>
2023-01-24 11:17:50 +0100

    added jump and obstacles

--- Assets/Scripts/LeftRightMovement.cs
+++ Assets/Scripts/LeftRightMovement.cs
@@ -7,6 +7,8 @@ public class LeftRightMovement : MonoBehaviour {
     private Rigidbody2D _rigidbody2D;
     [SerializeField] private float speed;
 
+    public float jumpForce;
+
     private void Start() {
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
@@ -15,5 +17,14 @@ public class LeftRightMovement : MonoBehaviour {
         float horizontalInput = Input.GetAxis(HorizontalInputAxisString);
         Vector2 movementDirection = Vector2.right * horizontalInput;
         _rigidbody2D.AddForce(movementDirection * (speed * Time.deltaTime), ForceMode2D.Impulse); // Reordered for performance
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
     }
+
+    private void Jump()
+    {
+        _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpForce);
+    }
+
 }
